11eb0a5d7f332f618322fc005758a856f2bc6e24
guhesse <hesse.gustavo@gmail.com>
2023-03-25 17:40:17 -0300

    add css & js on vantagens-charlie-sections

--- js/script.js
+++ js/script.js
@@ -16,3 +16,32 @@ function removeActiveClasses() {
         boxCity.classList.remove('active')
     })
 }
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("carrossel-item");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
